aec81f58cf3ce2768c94b1a1146760f0f6b4bf19
Jonas Dietrich <jonas.dietrich@uni-jena.de>
2021-10-13 11:29:39 +0200

    read in data

--- Analysis/main.py
+++ Analysis/main.py
@@ -2,12 +2,21 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-sal = pd.read_csv("Data/salinity.csv", names=["sal"])
-par = pd.read_csv("Data/par.csv", names=["par"])
-pressure = pd.read_csv("Data/pressure.csv", names=["pressure"])
-temp = pd.read_csv("Data/temperature.csv", names=["temp"])
-time = pd.read_csv("Data/time.csv", names=["time"])
+sal = pd.read_csv("Data/salinity.csv", names=["sal"], converters = {"sal":lambda x:[float(i) for i in x.split(";")]})
+#sal = pd.read_csv("Data/salinity.csv", names=["sal"], converters = {"sal":lambda x:x.split(";")})
+par = pd.read_csv("Data/par.csv", names=["par"],converters = {"par":lambda x:[float(i) for i in x.split(";")]})
+pressure = pd.read_csv("Data/pressure.csv", names=["pressure"],converters = {"pressure":lambda x:[float(i) for i in x.split(";")]})
+temp = pd.read_csv("Data/temperature.csv", names=["temp"],converters = {"temp":lambda x:[float(i) for i in x.split(";")]})
+time = pd.read_csv("Data/time.csv", names=["time"],converters = {"time":lambda x:[pd.to_datetime(float(i), unit="ns") for i in x.split(";")]})
 
 all_props = sal.join([par,pressure,temp,time])
-print(all_props.columns)
+exploded_all_props = all_props.explode("sal", ignore_index=True)
+print(exploded_all_props)
+#fig = plt.figure()
+#ax = plt.subplot()
+
+#ax.plot(all_props.time,all_props.sal.values)
+#plt.show()
+
