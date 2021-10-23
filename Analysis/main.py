63d3d0fd6fe69728cd8864675dd2988c4ba02650
Jonas Dietrich <jonasdietrich@Jonass-MacBook-Pro.local>
2021-10-23 17:21:23 +0200

    Corrected min cal (overall min added), log function for all var possible

--- Analysis/main.py
+++ Analysis/main.py
@@ -6,11 +6,21 @@ import numpy as np
 import gsw
 import random
 import os
-
+import matplotlib
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from windstress_plot import wind_stress
+
+chlorophyll_list = []
+with open("Data/chlorophyll.csv", "r") as file:
+    values = file.readlines()
+    for value in values:
+        value = value.replace("\n","")
+        for sub_value in value.split(";"):
+            chlorophyll_list.append(float(sub_value))
+min_chloro = np.nanmin(chlorophyll_list)
+
 
 sal = pd.read_csv("Data/salinity.csv", names=["sal"],
                   converters = {"sal":lambda x:[float(i) for i in x.split(";")]})
@@ -23,38 +33,17 @@ temp = pd.read_csv("Data/temperature.csv", names=["temp"],
 time = pd.read_csv("Data/time.csv", names=["time"],
                    converters = {"time":lambda x:[pd.to_datetime(float(i), unit="ns") for i in x.split(";")]})
 chloro = pd.read_csv("Data/chlorophyll.csv", names=["chloro"],
-                   converters = {"chloro":lambda x:[float(i) for i in x.split(";")]})
-
+               converters = {"chloro":lambda x:[float(i)+abs(min_chloro) for i in x.split(";")]})
 
 all_props = sal.join([par,pressure,temp,time,chloro])
 
-all_props["log_par"] = ""
-all_props["log_chloro"] = ""
+
 all_props["density"] = ""
 for i in range(len(all_props.sal)):
     all_props["density"][i] = gsw.sigma0(all_props.sal[i],all_props.temp[i])
-    # print(all_props.chloro[i])
-    # nan's zu 100 um später zu löschen
-    # log_chloro
-    no_nan_chloro = np.nan_to_num(all_props.chloro[i], nan=100)
-    # negative werte zu eins damit sie 0 werden
-    no_nan_chloro = np.where(no_nan_chloro<=0, 1000, no_nan_chloro)
-    log_values_chloro = np.log(no_nan_chloro)
-    #print(no_nan_chloro)
-    #print(log_values_chloro)
-    log_values_chloro = np.where(log_values_chloro==np.log(1000), np.nan, log_values_chloro)
-    all_props["log_chloro"][i] = np.where(log_values_chloro==np.log(100), np.nan, log_values_chloro)
-
-    # log_par
-    no_nan_par = np.nan_to_num(all_props.par[i], nan=10000)
-    no_nan_par = np.where(no_nan_par<=0, 100000, no_nan_par)
-    log_values_par = np.log(no_nan_par)
-    log_values_par = np.where(log_values_par==np.log(100000), np.nan, log_values_par)
-    all_props["log_par"][i] = np.where(log_values_par==np.log(10000), np.nan, log_values_par)
-
 
     
-def create_figure(x_values, y_values, color_values, colormap, values_to_label, i, num_sub):
+def create_figure(x_values, y_values, color_values, colormap, values_to_label, i, num_sub,log):
     '''creates a HEATMAP for sal, par, temp, pressure or density. The date is
     plotted on the pressure is plotted on the y-axis.'''
     stacked_color_values = np.column_stack((all_props[f"{color_values}"]))
@@ -62,13 +51,21 @@ def create_figure(x_values, y_values, color_values, colormap, values_to_label, i
     y_axis_values = [i[0] for i in y_values]
     y_axis_values = np.array([y_axis_values])[0,:]
     if i == None:
-        plot = ax.pcolormesh(y_axis_values,x_axis_values,stacked_color_values,cmap=f"{colormap}")
+        if log == "log":
+            plot = ax.pcolormesh(y_axis_values,x_axis_values,stacked_color_values,cmap=f"{colormap}",norm=matplotlib.colors.LogNorm(), shading="auto")
+        else:
+            plot = ax.pcolormesh(y_axis_values,x_axis_values,stacked_color_values,cmap=f"{colormap}", shading="auto")
+
         ax.invert_yaxis()
         plt.colorbar(plot,label=values_to_label[color_values])
         plt.ylabel("Pressure [dbar]")
         plt.xlabel("Time [d]")
     else:
-        plot = ax[i].contourf(y_axis_values,x_axis_values,stacked_color_values,cmap=f"{colormap}")
+        if log == "log":
+            plot = ax[i].contourf(y_axis_values,x_axis_values,stacked_color_values,cmap=f"{colormap}", norm=matplotlib.colors.LogNorm(), shading="auto")
+        else:
+            plot = ax[i].contourf(y_axis_values,x_axis_values,stacked_color_values,cmap=f"{colormap}", shading="auto")
+
         ax[i].invert_yaxis()
         divider = make_axes_locatable(ax[i])
         cax = divider.append_axes("right", size="3%", pad=0.5)
@@ -80,7 +77,7 @@ def create_figure(x_values, y_values, color_values, colormap, values_to_label, i
 
 
 def figure_layout(answer):
-    values_to_label = {"density":"Density [kg/m$^3$]", "chloro":"Chlorophyll [$\mu q$/L]", "temp":"Temperatur [$^\circ$C]", "par":"Photo. Active \n Radiation \n [$\mu q/cm² nm¹$]", "sal":"Salinity PSU", "wind":"Wind Stress [Pa]","log_par":"log PAR[$\mu q/cm² nm¹$]","log_chloro":"Chlorophyll log [$\mu q$/L]"}
+    values_to_label = {"density":"Density [kg/m$^3$]", "chloro":"Chlorophyll [$\mu q$/L]", "temp":"Temperatur [$^\circ$C]", "par":"PAR [$\mu q/cm² nm¹$]", "sal":"Salinity PSU", "wind":"Wind Stress [Pa]","par log":"PAR [$\mu q/cm² nm¹$]","chloro log":"Chlorophyll [$\mu g$/L]"}
     print("******************************** \n")
     for name_pair in values_to_label.items():
         print(f"{name_pair[0]} --> {name_pair[1]}")
@@ -89,9 +86,9 @@ def figure_layout(answer):
     if answer == "y":
         fig, ax = plt.subplots()
         i, num_sub = None, None
-        variable, colormap, im = choose_variable(i, num_sub)
+        variable, colormap, im, log = choose_variable(i, num_sub)
         if variable != "wind":
-            create_figure(all_props["pressure"].values, all_props["time"].values, variable, colormap, values_to_label, None, None)
+            create_figure(all_props["pressure"].values, all_props["time"].values, variable, colormap, values_to_label, None, None, log)
         plt.show()
     elif answer == "a":
         num_sub = int(input("Number of subplots:"))
@@ -99,17 +96,21 @@ def figure_layout(answer):
         plt.subplots_adjust(hspace=0.1)
         ax = ax.ravel()
         for i in range(num_sub):
-            variable, colormap, im = choose_variable(i, num_sub)
+            variable, colormap, im, log = choose_variable(i, num_sub)
             if variable == "wind":
                 pass
             else:    
-                create_figure(all_props["pressure"].values, all_props["time"].values, variable, colormap, values_to_label, i, num_sub)
+                create_figure(all_props["pressure"].values, all_props["time"].values, variable, colormap, values_to_label, i, num_sub, log)
         ax[i].set_xlabel("Time [d]")
         plt.show()
 
     
 def choose_variable(i, num_sub):
     variable = input("variable:")
+    try:
+        variable, log = variable.split(" ")
+    except ValueError:
+        log = None
     im, colormap = None, None
     if variable == "wind":
         wlength = input("Window Length !!must be an ODD number!! [default 21 -> press ENTER]:")
@@ -124,7 +125,7 @@ def choose_variable(i, num_sub):
             im = wind_stress(fig, ax,int(wlength), int(porder),i=i,num_sub=num_sub)
     else:
         colormap = input("colormap:")
-    return variable, colormap, im 
+    return variable, colormap, im, log
 
 
 if __name__ == "__main__":
